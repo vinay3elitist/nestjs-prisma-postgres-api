dc1d316f0691c6b3915d578284f70aca979d0011
vinay 3elitist <vinay.3elitist@gmail.com>
2024-06-06 15:33:05 +0530

    feat: added login profile update delete for user in controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,48 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-    @Post('register')
-    registerUser(@Body() createUserDto: CreateUserDto): string {
-        console.log(createUserDto);
-        return 'User registered';
-    }
+  @Post('register')
+  registerUser(@Body() createUserDto: CreateUserDto): string {
+    console.log(createUserDto);
+    return 'User registered';
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto): string {
+    console.log(loginUserDto);
+    return 'User logged in successfully';
+  }
+
+  @Get('profile')
+  profile(): string {
+    return 'User profile';
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): string {
+    console.log(updateUserDto);
+    return `Updated User ${id}`;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number): string {
+    return `Deleted User ${id}`;
+  }
 }
